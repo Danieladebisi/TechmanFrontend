fe72fb3ae004bcb568738c7f28a6fdd058ce56a7
Uwaoma Obinna Micheal <54563675+Rankshow@users.noreply.github.com>
2025-03-15 07:34:21 +0100

    modified the background colour of thee hamburger bar

--- src/components/NavbarMenu.jsx
+++ src/components/NavbarMenu.jsx
@@ -12,62 +12,59 @@ import "./NavbarMenu.css";
 function NavbarMenu({ admin, onLogout }) {
   return (
     <Navbar expand="lg" className="bg-teal-500 shadow-lg">
-      <Container fluid>
-        <Navbar.Brand href="/compare">
-          <img
-            src={Logo}
-            width="200"
-            height="80"
-            className="d-inline-block align-top"
-            alt="Techmansion logo"
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav className="mx-auto my-2 my-lg-0 gap-5" navbarScroll>
-            {/* <Link to="/" className="nav-link" style={{ color: "#3d85c6" }}>
-              Home
-            </Link> */}
-
-            <Link to="/compare" className="nav-link" style={{ color: "#3d85c6" }}>
-              Compare
-            </Link>
-            
-            <a
-              href="https://techmansion.tech/"
-              style={{ color: "#3d85c6" }}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="nav-link"
-            >
-              News
-            </a>
-            <a
-              href="https://techmansion.tech/about-tech-mansion/"
-              style={{ color: "#3d85c6" }}
-              target="_self"
-              rel="noopener noreferrer"
-              className="nav-link"
-            >
-              About
-            </a>
-            <Link to="/contact" className="nav-link" style={{ color: "#3d85c6" }}>
-              Contact
-            </Link>
-          </Nav> 
-
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+  <Container fluid>
+    <Navbar.Brand href="/compare">
+      <img
+        src={Logo}
+        width="200"
+        height="80"
+        className="d-inline-block align-top"
+        alt="Techmansion logo"
+      />
+    </Navbar.Brand>
+    <Navbar.Toggle
+      aria-controls="navbarScroll"
+      style={{ backgroundColor: "#3d85c6", borderColor: "ornage" }}
+    />
+    <Navbar.Collapse id="navbarScroll">
+      <Nav className="mx-auto my-2 my-lg-0 gap-5" navbarScroll>
+        <Link to="/compare" className="nav-link" style={{ color: "#3d85c6" }}>
+          Compare
+        </Link>
+        <a
+          href="https://techmansion.tech/"
+          style={{ color: "#3d85c6" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="nav-link"
+        >
+          News
+        </a>
+        <a
+          href="https://techmansion.tech/about-tech-mansion/"
+          style={{ color: "#3d85c6" }}
+          target="_self"
+          rel="noopener noreferrer"
+          className="nav-link"
+        >
+          About
+        </a>
+        <Link to="/contact" className="nav-link" style={{ color: "#3d85c6" }}>
+          Contact
+        </Link>
+      </Nav>
+      <Form className="d-flex">
+        <Form.Control
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+        />
+        <Button variant="outline-success">Search</Button>
+      </Form>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
   );
 }
 
